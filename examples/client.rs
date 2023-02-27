8a147da9cd1124c1a17b403ca9bec0322070e0d9
Miguel <miguel@fleek.xyz>
2023-02-27 13:28:07 -0500

    Fix logging

--- examples/client.rs
+++ examples/client.rs
@@ -1,10 +1,14 @@
 use anyhow::Result;
 use saga::client::Client;
 use tokio::main;
+use tracing::{subscriber, Level};
 
 #[main]
 async fn main() -> Result<()> {
-    env_logger::init();
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    subscriber::set_global_default(subscriber)?;
     let client = Client::new("127.0.0.1:8881".parse()?)?;
     client.get("somecid".to_string()).await
 }
--- examples/server.rs
+++ examples/server.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
 use tokio::main;
+use tracing::{subscriber, Level};
 
 #[main]
 async fn main() -> Result<()> {
-    env_logger::init();
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    subscriber::set_global_default(subscriber)?;
     saga::server::start("0.0.0.0:4455".parse()?).await?;
     Ok(())
 }
